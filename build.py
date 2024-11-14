bc7d9725d54cb73288d906554cd95212fd7c054f
nhubao <baoprokute1234@gmail.com>
2024-11-14 21:35:39 +0700

    Fix build

--- build.py
+++ build.py
@@ -14,6 +14,7 @@ import urllib.request
 # Keep original headers
 headers = {
     "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
 }
 
 
